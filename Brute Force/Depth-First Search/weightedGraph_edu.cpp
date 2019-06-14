e5a3670713460816dc995a57c8810948949ac2a4
rnjstjdgh <gshgsh1234@daum.net>
2019-06-14 18:33:59 +0900

    cpp알고리즘 추가

---
+++ Brute Force/Depth-First Search/weightedGraph_edu.cpp
@@ -0,0 +1,66 @@
+//weightedGraph
+
+#include "algorithm-visualizer.h"
+#include <iostream>
+
+const int				GRAPHSIZE = /*(그래프 사이즈 입력) */
+//visualizer{
+GraphTracer				tracer("GraphTracer");
+LogTracer				logger("LogTracer");
+Randomize::Graph<int>	graph(GRAPHSIZE, 1);
+int						G[GRAPHSIZE][GRAPHSIZE];
+bool					D[GRAPHSIZE];
+
+void init_DepthFirstSearch_weightedGraph()
+{
+	Layout::setRoot(VerticalLayout({ tracer,logger }));
+	tracer.log(logger);
+	tracer.weighted();
+	tracer.directed(false);
+	graph.directed(false);
+	graph.weighted();
+	int g[GRAPHSIZE*GRAPHSIZE];
+	graph.fill(g);
+	for (int i = 0; i < GRAPHSIZE; i++) {
+		for (int j = 0; j < GRAPHSIZE; j++)
+			G[i][j] = g[i *GRAPHSIZE + j];
+	}
+	tracer.set(G);
+	Tracer::delay();
+}
+//}
+
+void DFS(int node, int parent, int weight)
+{
+	//visualizer{
+	tracer.visit(node, parent, weight);
+	Tracer::delay();
+	//}
+	D[node] = true; 
+	for (int i = 0; i < GRAPHSIZE; i++) {
+		if (G[node][i]) {
+			if (/*(알맞은 조건을 입력) */) {
+				DFS(i, node, /*(알맞은 값 입력) */);
+			}
+		}
+	}
+	D[node] = /*(알맞은 값 입력) */ 
+	//visualizer{
+	tracer.leave(node, parent, 0);
+	Tracer::delay();
+	//}
+}
+
+int main()
+{
+	init_DepthFirstSearch_weightedGraph();
+	for (int k = 0; k < GRAPHSIZE; k++) {
+		//visualizer{
+		logger.println("start from " + std::to_string(k));
+		//}
+		for (int i = 0; i < GRAPHSIZE; i++)
+			D[i] = false;
+		DFS(k, NULL, 0);
+
+	}
+}
